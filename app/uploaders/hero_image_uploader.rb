330572d3fd62b26936449c0c00ea60afd693e4ba
Dan Brown <dbrown@occameducation.com>
2020-10-28 13:15:57 -0500

    Resize thumbnails for hero images

--- app/uploaders/hero_image_uploader.rb
+++ app/uploaders/hero_image_uploader.rb
@@ -17,7 +17,9 @@ class HeroImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process :resize_to_limit => [960, 960]
+  version :thumb do
+    process resize_to_fill: [375, 225]
+  end
 
   # Returns URL for image-asset
   def default_url
