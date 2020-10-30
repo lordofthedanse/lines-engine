4a8a0ddafecf186e627a73c1568d0c00ce963847
Dan Brown <dbrown@occameducation.com>
2020-10-30 03:20:16 -0500

    Update 20140505122014_base_migration.rb

--- db/migrate/20140505122014_base_migration.rb
+++ db/migrate/20140505122014_base_migration.rb
@@ -1,84 +1,89 @@
-class BaseMigration < ActiveRecord::Migration[4.2]
-  # encoding: UTF-8
-  # This file is auto-generated from the current state of the database. Instead
-  # of editing this file, please use the migrations feature of Active Record to
-  # incrementally modify your database, and then regenerate this schema definition.
-  #
-  # Note that this schema.rb definition is the authoritative source for your
-  # database schema. If you need to create the application database on another
-  # system, you should be using db:schema:load, not running all the migrations
-  # from scratch. The latter is a flawed and unsustainable approach (the more migrations
-  # you'll amass, the slower it'll run and the greater likelihood for issues).
-  #
-  # It's strongly recommended that you check this file into your version control system.
-
+class BaseMigration < ActiveRecord::Migration[6.0]
   def change
 
-    create_table "lines_articles", force: true do |t|
-      t.string   "title"
-      t.string   "sub_title"
-      t.text     "content"
-      t.boolean  "published",        default: false
-      t.datetime "published_at"
-      t.string   "hero_image"
-      t.datetime "created_at",                       null: false
-      t.datetime "updated_at",                       null: false
-      t.string   "slug"
-      t.string   "gplus_url"
-      t.boolean  "featured",         default: false
-      t.string   "document"
-      t.string   "short_hero_image"
+    create_table :lines_articles, id: :uuid do |t|
+      t.string :title
+      t.string :sub_title
+      t.text :content
+      t.text :teaser
+      t.boolean :published, default: false
+      t.datetime :published_at
+      t.text :hero_image_data
+      t.string :slug
+      t.boolean :featured, default: false
+      t.text :document_data
+
+      t.timestamps
     end
 
-    add_index "lines_articles", ["slug"], name: "index_lines_articles_on_slug", unique: true, using: :btree
+    add_index :lines_articles, :slug, name: "index_lines_articles_on_slug", unique: true
 
-    create_table "lines_authorables", force: true do |t|
-      t.integer  "author_id"
-      t.integer  "article_id"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
+    create_table "lines_authorables", id: :uuid do |t|
+      t.uuid :author_id, null: false
+      t.uuid :article_id, null: false
+
+      t.timestamps
     end
 
-    add_index "lines_authorables", ["article_id"], name: "index_lines_authorables_on_article_id", using: :btree
-    add_index "lines_authorables", ["author_id"], name: "index_lines_authorables_on_author_id", using: :btree
+    add_index :lines_authorables, :article_id, name: "index_lines_authorables_on_article_id"
+    add_index :lines_authorables, :author_id, name: "index_lines_authorables_on_author_id"
 
-    create_table "lines_authors", force: true do |t|
-      t.string   "name"
-      t.string   "email"
-      t.datetime "created_at",    null: false
-      t.datetime "updated_at",    null: false
-      t.text     "description"
-      t.string   "gplus_profile"
+    create_table :lines_authors, id: :uuid do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.text :description, null: false
+
+      t.timestamps
     end
 
-    create_table "friendly_id_slugs", force: true do |t|
-      t.string   "slug",                      null: false
-      t.integer  "sluggable_id",              null: false
-      t.string   "sluggable_type", limit: 40
-      t.datetime "created_at"
+    create_table :friendly_id_slugs, id: :uuid do |t|
+      t.string :slug, null: false
+      t.uuid :sluggable_id, null: false
+      t.string :sluggable_type, limit: 40
+
+      t.timestamps
     end
 
-    add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", unique: true, using: :btree
-    add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
-    add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+    add_index :friendly_id_slugs, [:slug, :sluggable_type], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", unique: true
+    add_index :friendly_id_slugs, :sluggable_id, name: "index_friendly_id_slugs_on_sluggable_id"
+    add_index :friendly_id_slugs, :sluggable_type, name: "index_friendly_id_slugs_on_sluggable_type"
 
-    create_table "lines_pictures", force: true do |t|
-      t.string   "image"
-      t.string   "name"
-      t.integer  "article_id"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
+    create_table :lines_pictures, id: :uuid do |t|
+      t.text :image_data, null: false
+      t.string :name
+      t.uuid :article_id, null: false
+      t.timestamps
     end
 
-    add_index "lines_pictures", ["article_id"], name: "index_lines_pictures_on_article_id", using: :btree
+    add_index :lines_pictures, :article_id, name: "index_lines_pictures_on_article_id"
 
+    create_table :lines_users, id: :uuid do |t|
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :reset_digest
+      t.datetime :reset_sent_at
 
-    create_table "lines_users", force: true do |t|
-      t.string   "email"
-      t.string   "password_digest"
-      t.datetime "created_at",      null: false
-      t.datetime "updated_at",      null: false
+      t.timestamps
     end
 
+    create_table :tags, id: :uuid do |t|
+      t.string :name
+      t.integer :taggings_count, default: 0
+
+      t.timestamps
+    end
+
+    add_index :tags, :name, unique: true
+
+    create_table :taggings, id: :uuid do |t|
+      t.references :tag, type: :uuid
+      t.references :taggable, type: :uuid, polymorphic: true
+      t.references :tagger, type: :uuid, polymorphic: true
+      t.string :context, limit: 128
+
+      t.timestamps
+    end
+
+    add_index :taggings, [:tag_id, :taggable_id, :taggable_type, :context, :tagger_id, :tagger_type], unique: true, name: 'taggings_idx'
   end
 end
