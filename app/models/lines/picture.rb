187191e3aea3061a91cada5fe18bd44ce8508455
Dan Brown <dbrown@occameducation.com>
2020-10-30 03:25:03 -0500

    Update picture.rb

--- app/models/lines/picture.rb
+++ app/models/lines/picture.rb
@@ -1,17 +1,9 @@
-# The content of an {Article} can contain multiple pictures.
-# Pictures are associated to an article through parsing the articles
-# content and extracting the ids of the used images.
-
-require 'carrierwave'
-require 'carrierwave/orm/activerecord'
-
 module Lines  
   class Picture < Lines::ApplicationRecord
+    include PictureUploader[:picture]
+    
     # Associations    
     belongs_to :article, touch: true, optional: true
-
-    # Mount carrierwave picture uploader
-    mount_uploader :image, PictureUploader
 
     # Callbacks
     before_create :default_name
